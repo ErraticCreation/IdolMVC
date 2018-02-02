069701be4ceb26f5b601f05c5b33c0dd2d97676f
Tarah Nixon <ErraticCreation@gmail.com>
2018-02-02 17:31:43 +0000

    More fleshing out Core.php and created the default (Pages)controller and (Index)method for the root/homepage

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -24,11 +24,31 @@
       
       // Require the controller
       require_once '../app/controllers/'. $this->currentController . '.php';
+      
+      // Instantiate the controller class
+      $this->currentController = new $this->currentController;
+      
+      // Check for the second part of the URL
+      if(isset($url[1])) {
+        // Check to see if the method exists within the controller
+        if(method_exists($this->currentController, $url[1])) {
+          $this->currentMethod = $url[1];
+          // Unset the 1 index
+          unset($url[1]);
+        }
+      }
+      
+      // Get params
+      $this->params = $url ? array_values($url) : [];
+      
+      // Call a callback with the array of params
+      call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
     
     public function getUrl() {
       if(isset($_GET['url'])) {
         $url = rtrim($_GET['url'], '/');
+        $url = strtolower($url);
         $url = filter_var($url, FILTER_SANITIZE_URL);
         $url = explode('/', $url);
         return $url;
